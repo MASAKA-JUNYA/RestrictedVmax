859354dfc8987b673a83f052bdef5350f133e5ed
真坂 順也 <masaka.junya.283@s.kyushu-u.ac.jp>
2021-11-24 14:16:39 +0900

    deleted some data

--- 新しいフォルダー/Multilane_TrafficFlow.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Update_Position.h"
-
-class Multilane_TrafficFlow {
-private:
-	void _DoSimulation(Update_Position* DoSim);
-public:
-	void Calculation(int filenumber, int lanelength, int Numberofvehicle, int Numberoflane);
-};
